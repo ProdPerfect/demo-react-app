c92114f9ee06d75ba654e46a811b83098bedca6b
Anton de Winter <adewinter@gmail.com>
2019-03-12 12:47:51 -0600

    Add gh-pages dep and try to publish to github pages

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
 const prodPerfectConfig = {
-    host: '<PROD PERFECT HOST URL>'
-    libraryUrl: '<keen-tracking.min.js LIBRARY URL>',
-    projectId: '<YOUR PROJECT ID>',
-    writeKey: '<YOUR WRITE KEY>',
+    host: 'pevnertest.datapipe.prodperfect.com/v1',
+    libraryUrl: 'https://pevnertest.trackinglibrary.prodperfect.com/keen-tracking.min.js',
+    projectId: '3tq1WVDi5zBF08w3EqFCLK11',
+    writeKey: 'DQZGHGI5JRHCIIL249LR954YXNDL6IWJOYIRBX66TXY4FXFWTMOR4LCJ77WKESG59WTTADQB7BKGCUVPS0U1A0X13NI6EWQEKUY8LOPZDG5HTI8DXUB933S2TWOL7QJ5EI0P2PQUNED67TL1XOB3EBKM4QHLTH66GESTSWGXQMAZRHLS8TXMGIS260NELCIR'
 };
 
 (function(e){var n,r="ProdPerfectKeen",o=!!window.Keen&&window.Keen;window[r]=window[r]||{ready:function(t){var a,d=document.getElementsByTagName("head")[0],i=document.createElement("script"),c=window;i.onload=i.onreadystatechange=function(){if(!(i.readyState&&!/^c|loade/.test(i.readyState)||a)){if(i.onload=i.onreadystatechange=null,a=1,n=c.Keen,o)c.Keen=o;else try{delete c.Keen}catch(e){c.Keen=void 0}c[r]=n,c[r].ready(t)}},i.async=1,i.src=e.libraryUrl,d.parentNode.insertBefore(i,d)}},window[r].ready(function(){e.requestType="beacon";var n=new window.ProdPerfectKeen(e);n.extendEvents({visitor:{user_id:null}});n.initAutoTracking({ignoreDisabledFormFields:!1,recordClicks:!0,recordFormSubmits:!0,recordInputChanges:!0,recordPageViews:!0,recordPageUnloads:!0,recordScrollState:!0})})})(prodPerfectConfig); // eslint-disable-line
