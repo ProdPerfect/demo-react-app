52d23839eb6c8143ccabe2f01d48bc6eab9c8208
Anton de Winter <adewinter@gmail.com>
2019-03-12 12:28:06 -0600

    Updates

---
+++ precache-manifest.be52ccfdbdc3cf09d3fb60543f965dac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a478909665db37fce30f13a8470cd3a",
+    "url": "/demo-react-app/static/media/p2-color-logo.8a478909.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "732561615dd4938a4015",
+    "url": "/demo-react-app/static/js/runtime~main.4a9203b2.js"
+  },
+  {
+    "revision": "7cfc1d9b4f7032e136cd",
+    "url": "/demo-react-app/static/js/main.c3248524.chunk.js"
+  },
+  {
+    "revision": "a33238adbc15d0a18ae3",
+    "url": "/demo-react-app/static/js/2.b4fcef41.chunk.js"
+  },
+  {
+    "revision": "7cfc1d9b4f7032e136cd",
+    "url": "/demo-react-app/static/css/main.7dd6f1bd.chunk.css"
+  },
+  {
+    "revision": "a40cda259b8f1a0a66a282e4d4c65f96",
+    "url": "/demo-react-app/index.html"
+  }
+];
